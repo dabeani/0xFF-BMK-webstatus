b6159e285f98148ac5684680fdca31e34f706249
pocki80 <pocki@pocki.at>
2016-11-10 12:36:49 +0100

    final fixes

--- config/custom/www/cgi-bin-status.php
+++ config/custom/www/cgi-bin-status.php
@@ -312,8 +312,8 @@ flush();
 					<li role="presentation"><a href="#"><?php echo  $APP["ip"] ." - ".$APP["hostname"]; ?></a></li>
 				</ul><br>
 <?
-$APP["devices"] = explode("\n",shell_exec("/usr/sbin/ubnt-discover -d150 -i \"".$interface_1100_list."\""));
-$APP["devices"][0]=str_replace("Local","Local  ",$APP["devices"][0]);
+//$APP["devices"] = explode("\n",shell_exec("/usr/sbin/ubnt-discover -d150 -i \"".$interface_1100_list."\""));
+//$APP["devices"][0]=str_replace("Local","Local  ",$APP["devices"][0]);
 $APP["v4defaultrouteviaport"] = trim(shell_exec("ip r | grep default | awk {'sub(/^eth0./,\"\",$5);print $5'}"));
 $APP["v6defaultrouteviaport"] = trim(shell_exec("ip -f inet6 r | grep default | awk {'sub(/^eth0./,\"\",$5);print $5'}"));
 $APP["ipv6_status"] = trim(shell_exec("netstat -na | grep 2008"));
@@ -340,12 +340,11 @@ $APP["ipv6_status"] = trim(shell_exec("netstat -na | grep 2008"));
 						<dl class="dl-horizontal">
 						  <dt>System Uptime <span class="glyphicon glyphicon-time" aria-hidden="true"></span></dt><dd><?php echo shell_exec("uptime") ?></dd>
 						  <dt>IPv4 Default-Route <span class="glyphicon glyphicon-transfer" aria-hidden="true"></span></dt><dd><?php echo "<a href=\"https://".$APP["v4defaultrouteviadns"]."\">".$APP["v4defaultrouteviadns"]." (".$APP["v4defaultrouteviaip"].")</a> via ".$APP["v4defaultrouteviaport"]."<br>"; ?></dd>
-						  <dt>mgmt Devices <span class="glyphicon glyphicon-signal" aria-hidden="true"></span></dt><dd><pre><?php echo implode("\n", $APP["devices"]); ?></pre></dd>
 						  <dt>mgmt Devices <span class="glyphicon glyphicon-signal" aria-hidden="true"></span></dt><dd>
-						  <? /* NEW DEVICES START */
+						  <?
 						  $APP["devices_list"]=json_decode(shell_exec("/usr/sbin/ubnt-discover -d150 -V -i \"".$interface_1100_list."\" -j"),true);
 						  if (count($APP["devices_list"]>0)) {
-							echo "<table class=\"table table-hover table-bordered table-condensed\"><thead><tr valign=top><td><b>Hardware Address</b></td><td><b>Local IP</b></td><td><b>Hostname</b></td>";
+							echo "<table class=\"table table-hover table-bordered table-condensed\"><thead><tr valign=top><td><b>HW Address</b></td><td><b>Local IP</b></td><td><b>Hostname</b></td>";
 							echo "<td><b>Product</b></td><td><b>Uptime</b></td><td><b>WMODE</b></td><td><b>ESSID</b></td><td><b>Firmware</b></td></tr></thead>\n";
 							echo "<tbody>\n";
 							foreach ($APP["devices_list"] as $device) {
@@ -366,7 +365,6 @@ $APP["ipv6_status"] = trim(shell_exec("netstat -na | grep 2008"));
 						  } else {
 							echo "No devices discovered";
 						  }
-						  /* NEW DEVICES END */ 
 						  ?>
 						  </dd>
 						  <dt>IPv4 OLSR-Links <span class="glyphicon glyphicon-link" aria-hidden="true"></span></dt><dd><?php echo getOLSRLinks(); ?></dd>

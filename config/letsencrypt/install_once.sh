7e19c95473b0c00eee04403d88bf78afdf08765c
pocki80 <pocki@pocki.at>
2017-01-29 20:07:41 +0100

    Update install_once.sh

--- config/letsencrypt/install_once.sh
+++ config/letsencrypt/install_once.sh
@@ -6,11 +6,8 @@
 # Input regarding FQDN which will be used
 read -p "Enter your full FQDN:" fqdn
 
-# correctoin permissions, create cron-job
-chmod 755 /config/letsencrypt/acme_tiny.py
-chmod 755 /config/letsencrypt/letsrenew.sh
+# create cron-job
 ln -sf /config/letsencrypt/letsrenew.sh /etc/cron.monthly/letsrenew.sh
-chmod 755 /config/scripts/post-config.d/install_letsencrypt.sh
 
 # Generate certifications which will be used
 openssl genrsa 4096 | tee /config/letsencrypt/account.key

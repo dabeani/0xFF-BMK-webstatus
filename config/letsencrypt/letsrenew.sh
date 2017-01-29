cbe9db74aca30824c12beddb2a7a78a0c8645a95
pocki80 <pocki@pocki.at>
2017-01-29 18:37:29 +0100

    Update letsrenew.sh

--- config/letsencrypt/letsrenew.sh
+++ config/letsencrypt/letsrenew.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+# Opening up firewall on port 80
+CHAIN=$( iptables -L | awk '/^Chain WAN/ && /LOCAL/ {print $2;}' )
+iptables -I $CHAIN 1 -p tcp --dport 80 -j ACCEPT
+
+# Run renewal script
+python /config/letsencrypt/acme_tiny.py --account-key /config/letsencrypt/account.key --csr /config/letsencrypt/domain.csr --acme-dir /config/custom/www/.well-known/acme-challenge/ > /config/letsencrypt/signed.crt
+
+# Removing firewall rule added earlier
+iptables -D $CHAIN 1
+
+# Copying files to lighttpd directory
+cat /config/letsencrypt/signed.crt | tee /etc/lighttpd/server.pem
+cat /config/letsencrypt/domain.key | tee -a /etc/lighttpd/server.pem
+
+# Restarting lighttpd daemon
+ps -e | grep lighttpd | awk '{print $1;}' | xargs kill
+/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf
+/usr/sbin/lighttpd -f /config/custom/lighttpd/lighttpd_custom.conf

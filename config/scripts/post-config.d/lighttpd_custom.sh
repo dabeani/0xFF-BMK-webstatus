816802136974ae459a4282298244efe539790dba
Bernhard M <bernhard@cybercomm.at>
2017-01-30 19:43:46 +0100

    added original EsgeOS webserver start procedure

--- config/scripts/post-config.d/lighttpd_custom.sh
+++ config/scripts/post-config.d/lighttpd_custom.sh
@@ -6,7 +6,12 @@ then
     chown www-data:www-data /var/log/lighttpd_custom
 fi
 
+# start custom script webserver
 sudo /sbin/start-stop-daemon --start --quiet \
 	--pidfile /var/run/lighttpd_custom.pid \
 	--exec /usr/sbin/lighttpd -- -f /config/custom/lighttpd/lighttpd_custom.conf
 
+# start original EsgeOS webserver
+sudo /sbin/start-stop-daemon --start --quiet \
+	--pidfile /var/run/lighttpd.pid \
+	--exec /usr/sbin/lighttpd -- -f /config/custom/lighttpd/lighttpd.conf

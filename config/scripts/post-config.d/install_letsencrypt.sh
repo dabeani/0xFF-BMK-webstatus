de5f04573716218d039a7877701093b68e465afa
Bernhard M <bernhard@cybercomm.at>
2017-01-30 19:24:37 +0100

    update webserver(s) restart procedure

--- config/scripts/post-config.d/install_letsencrypt.sh
+++ config/scripts/post-config.d/install_letsencrypt.sh
@@ -7,7 +7,9 @@ ln -sf /config/letsencrypt/letsrenew.sh /etc/cron.monthly/letsrenew.sh
 cat /config/letsencrypt/signed.crt | tee /etc/lighttpd/server.pem
 cat /config/letsencrypt/domain.key | tee -a /etc/lighttpd/server.pem
 
-# restart both lighttpd instances
-ps -e | grep lighttpd | awk '{print $1;}' | xargs kill
+# Restarting original lighttpd webserver for EdgeOS
+ps aux | grep lighttpd.conf | awk '{print $2;}' | head -n1 | xargs kill
 /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf
-/usr/sbin/lighttpd -f /config/custom/lighttpd/lighttpd_custom.conf
+# Restart custom lighttpd webserver for custom scripts
+ps aux | grep lighttpd_custom.conf | awk '{print $2;}' | head -n1 | xargs kill
+/usr/sbin/lighttpd -f /config/custom/lighttpd/lighttpd_custom.con

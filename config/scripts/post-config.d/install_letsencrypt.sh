6820aeef18184606778679eae9572fff861342c5
pocki80 <pocki@pocki.at>
2017-01-31 09:46:19 +0100

    removed all content (can be deleted later)

--- config/scripts/post-config.d/install_letsencrypt.sh
+++ config/scripts/post-config.d/install_letsencrypt.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-# re-establish monthly renewal
-ln -sf /config/letsencrypt/letsrenew.sh /etc/cron.monthly/letsrenew.sh
-
-# re-establish current certificate file
-cat /config/letsencrypt/signed.crt | tee /etc/lighttpd/server.pem
-cat /config/letsencrypt/domain.key | tee -a /etc/lighttpd/server.pem
-
-# Restarting original lighttpd webserver for EdgeOS
-ps aux | grep lighttpd.conf | awk '{print $2;}' | head -n1 | xargs kill
-/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf
-# Restart custom lighttpd webserver for custom scripts
-ps aux | grep lighttpd_custom.conf | awk '{print $2;}' | head -n1 | xargs kill
-/usr/sbin/lighttpd -f /config/custom/lighttpd/lighttpd_custom.con
+#
+#covered withing /config/scripts/post-config.d/lighttpd_custom.sh

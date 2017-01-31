ffe132ef66f24dd543b8bb3ec06eaa78d7dade88
pocki80 <pocki@pocki.at>
2017-01-31 09:45:37 +0100

    implemented install_letsencrypt.sh, check against domain.key filesize

--- config/scripts/post-config.d/lighttpd_custom.sh
+++ config/scripts/post-config.d/lighttpd_custom.sh
@@ -1,17 +1,35 @@
 #!/bin/sh
 
-if [ ! -d "/var/log/lighttpd_custom" ]
-then
-    mkdir /var/log/lighttpd_custom
-    chown www-data:www-data /var/log/lighttpd_custom
+# create missing log-directory if needed
+if [ ! -d "/var/log/lighttpd_custom" ]; then
+  mkdir /var/log/lighttpd_custom
+  chown www-data:www-data /var/log/lighttpd_custom
 fi
 
-# start custom script webserver
-sudo /sbin/start-stop-daemon --start --quiet \
-	--pidfile /var/run/lighttpd_custom.pid \
-	--exec /usr/sbin/lighttpd -- -f /config/custom/lighttpd/lighttpd_custom.conf
+# re-establish monthly renewal
+ln -sf /config/letsencrypt/letsrenew.sh /etc/cron.monthly/letsrenew.sh
 
-# start original EsgeOS webserver
-sudo /sbin/start-stop-daemon --start --quiet \
-	--pidfile /var/run/lighttpd.pid \
-	--exec /usr/sbin/lighttpd -- -f /etc/lighttpd/lighttpd.conf
+# re-establish current certificate file, only of domain.key is not of zero file-size
+if [ -f "/config/letsencrypt/domain.key" ] && [ ! $(stat -c %s /config/letsencrypt/domain.key) -eq 0 ]
+  then
+  cat /config/letsencrypt/signed.crt | tee /etc/lighttpd/server.pem
+  cat /config/letsencrypt/domain.key | tee -a /etc/lighttpd/server.pem
+ 
+  # Restarting original lighttpd webserver for EdgeOS
+  sudo /sbin/start-stop-daemon --stop --pidfile /var/run/lighttpd.pid
+  if [ -f "/var/run/lighttpd.pid" ]; then
+    rm /var/run/lighttpd.pid
+  fi
+  sudo /sbin/start-stop-daemon --start --quiet \
+        --pidfile /var/run/lighttpd.pid \
+        --exec /usr/sbin/lighttpd -- -f /etc/lighttpd/lighttpd.conf
+  
+  # Restart custom lighttpd webserver for custom scripts
+  sudo /sbin/start-stop-daemon --stop --pidfile /var/run/lighttpd_custom.pid
+  if [ -f "/var/run/lighttpd_custom.pid" ]; then
+    rm /var/run/lighttpd_custom.pid
+  fi
+  sudo /sbin/start-stop-daemon --start --quiet \
+        --pidfile /var/run/lighttpd_custom.pid \
+        --exec /usr/sbin/lighttpd -- -f /config/custom/lighttpd/lighttpd_custom.conf
+fi

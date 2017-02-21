fbab42a9ce9395b01ede4ba74a125c473a9cba5c
pocki80 <pocki@pocki.at>
2017-02-21 08:49:26 +0100

    Update port_8443.sh

--- config/scripts/port_8443.sh
+++ config/scripts/port_8443.sh
@@ -1,7 +1,10 @@
 #!/bin/vbash
     
 source /opt/vyatta/etc/functions/script-template
-    
+session_env=`/bin/cli-shell-api getSessionEnv $PPID`
+eval $session_env
+cli-shell-api setupSession
+
 configure
 set service gui https-port 8443
 commit

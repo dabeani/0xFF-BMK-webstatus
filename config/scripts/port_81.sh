fa31c0694687c90a230b06dff5c734f575541112
pocki80 <pocki@pocki.at>
2017-02-21 08:49:07 +0100

    Update port_81.sh

--- config/scripts/port_81.sh
+++ config/scripts/port_81.sh
@@ -1,7 +1,10 @@
 #!/bin/vbash
     
 source /opt/vyatta/etc/functions/script-template
-    
+session_env=`/bin/cli-shell-api getSessionEnv $PPID`
+eval $session_env
+cli-shell-api setupSession
+
 configure
 set service gui http-port 81
 commit

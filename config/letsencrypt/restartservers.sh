2cae41b0b1c9dc2c714df5e27768c0cdc739ec9f
pocki80 <pocki@pocki.at>
2017-04-14 13:52:07 +0200

    remove CR characters

--- config/letsencrypt/restartservers.sh
+++ config/letsencrypt/restartservers.sh
@@ -4,7 +4,6 @@ echo "$(date +%H:%M:%S.%N) Restarting daemons" >>/var/log/0xwslerestart.log
 sudo /sbin/start-stop-daemon --stop --pidfile /var/run/lighttpd_custom.pid >>/var/log/0xwslerestart.log 2>>/var/log/0xwslerestart.log
 sudo /sbin/start-stop-daemon --stop --pidfile /var/run/lighttpd.pid >>/var/log/0xwslerestart.log 2>>/var/log/0xwslerestart.log
 sleep 1
-
 sudo /sbin/start-stop-daemon --start --quiet --pidfile /var/run/lighttpd.pid --exec /usr/sbin/lighttpd -- -f /etc/lighttpd/lighttpd.conf >>/var/log/0xwslerestart.log 2>>/var/log/0xwslerestart.log
 sudo /sbin/start-stop-daemon --start --quiet --pidfile /var/run/lighttpd_custom.pid --exec /usr/sbin/lighttpd -- -f /config/custom/lighttpd/lighttpd_custom.conf >>/var/log/0xwslerestart.log 2>>/var/log/0xwslerestart.log
 exit 0

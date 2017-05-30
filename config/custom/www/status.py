2026702e19f199bbaef597984edc264ff30fa80b
pocki80 <pocki@pocki.at>
2017-05-30 14:26:38 +0200

    add some olsr data

--- config/custom/www/status.py
+++ config/custom/www/status.py
@@ -39,10 +39,15 @@ def show_status():
 
 def show_html():
     # get ubnt-discover
-    exec_command="/usr/sbin/ubnt-discover -d150 -V -i br0.1100,br1,br1.1100,eth0.1100,eth1.1100,eth2.1100,eth3.1100,eth4.1100"
+    exec_command="/usr/sbin/ubnt-discover -d150 -i br0.1100,br1,br1.1100,eth0.1100,eth1.1100,eth2.1100,eth3.1100,eth4.1100"
     args = shlex.split(exec_command)
     data = subprocess.check_output(args)
 
+    # get local olsr infos
+    import urllib2
+    olsr_links = urllib2.urlopen("http://127.0.0.1:2006/links").read()
+
+    # start to print content
     print("Content-Type: text/plain")
     print("X-Powered-By: cpo/bmk-v4.7")
     print         # blank line, end of headers
@@ -51,6 +56,7 @@ def show_html():
     print
     print 
     print "### OLSR-NEIGHBORS ###"
+    print olsr_links
 
 if (GET.get('get') == "status"):
     show_status()

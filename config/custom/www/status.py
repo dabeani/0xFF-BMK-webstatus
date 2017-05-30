36b03531c7a70b6d323f95a3938a543abe54649d
pocki80 <pocki@pocki.at>
2017-05-30 09:44:29 +0200

    Add files via upload

--- config/custom/www/status.py
+++ config/custom/www/status.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+
 import shlex, subprocess
-exec_command="/usr/sbin/ubnt-discover -d150 -V -i br0.1100,br1 -j"
+exec_command="/usr/sbin/ubnt-discover -d150 -V -i br0.1100,br1,br1.1100,eth0.1100,eth1.1100,eth2.1100,eth3.1100,eth4.1100 -j"
 args = shlex.split(exec_command)
+
+print
 print subprocess.check_output(args)

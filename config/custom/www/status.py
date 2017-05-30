2c5c5d7b3a82f32278c94c89cb9352ad6d1962d6
pocki80 <pocki@pocki.at>
2017-05-30 14:14:04 +0200

    status as json or olsr as html

--- config/custom/www/status.py
+++ config/custom/www/status.py
@@ -1,21 +1,59 @@
 #!/usr/bin/env python
 
 import shlex, subprocess
-import json
+import json, os
 
-# get ubnt-discover
-exec_command="/usr/sbin/ubnt-discover -d150 -V -i br0.1100,br1,br1.1100,eth0.1100,eth1.1100,eth2.1100,eth3.1100,eth4.1100 -j"
-args = shlex.split(exec_command)
-data = json.loads(subprocess.check_output(args))
+# get http-get variables
+GET={}
+if (os.getenv("QUERY_STRING") is None):
+    GET["get"]="default"
+else:
+    args=os.getenv("QUERY_STRING").split('&')
+    for arg in args:
+        t=arg.split('=')
+        if len(t)>1: k,v=arg.split('='); GET[k]=v
 
-# get versions of wizards and IP-addresses from seperate shellscript
-versions = json.loads(subprocess.check_output("/config/custom/versions.sh"))
-# merge it to data array
-data["wizards"]=versions["wizards"]
-data["local_ips"]=versions["local_ips"]
+if (GET.get('get') is None):
+    GET["get"]="default"
 
-# return json output
-print("Content-Type: text/json")
-print("X-Powered-By: cpo/bmk-v4.7")
-print         # blank line, end of headers
-print json.dumps(data)
+if (GET.get('get') == ""):
+    GET["get"]="default"
+
+def show_status():
+    # get ubnt-discover
+    exec_command="/usr/sbin/ubnt-discover -d150 -V -i br0.1100,br1,br1.1100,eth0.1100,eth1.1100,eth2.1100,eth3.1100,eth4.1100 -j"
+    args = shlex.split(exec_command)
+    data = json.loads(subprocess.check_output(args))
+
+    # get versions of wizards and IP-addresses from seperate shellscript
+    versions = json.loads(subprocess.check_output("/config/custom/versions.sh"))
+    # merge it to data array
+    data["wizards"]=versions["wizards"]
+    data["local_ips"]=versions["local_ips"]
+
+    # return json output
+    print("Content-Type: text/json")
+    print("X-Powered-By: cpo/bmk-v4.7")
+    print         # blank line, end of headers
+    print json.dumps(data)
+
+def show_html():
+    # get ubnt-discover
+    exec_command="/usr/sbin/ubnt-discover -d150 -V -i br0.1100,br1,br1.1100,eth0.1100,eth1.1100,eth2.1100,eth3.1100,eth4.1100"
+    args = shlex.split(exec_command)
+    data = subprocess.check_output(args)
+
+    print("Content-Type: text/plain")
+    print("X-Powered-By: cpo/bmk-v4.7")
+    print         # blank line, end of headers
+    print "### UBNT-DISCOVER ###"
+    print data
+    print
+    print 
+    print "### OLSR-NEIGHBORS ###"
+
+if (GET.get('get') == "status"):
+    show_status()
+else:
+    show_html()
+

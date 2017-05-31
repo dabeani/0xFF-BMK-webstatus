035c0296735a409b50566f6c53934466ab6e3ebd
pocki80 <pocki@pocki.at>
2017-05-31 11:50:37 +0200

    fix

--- config/custom/www/status.py
+++ config/custom/www/status.py
@@ -222,7 +222,7 @@ def show_html():
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-        <h4 class="modal-title" id="myModalLabel">"""+len(destinationlist)+""" routes via <b>"""+key+"""</b></h4>
+        <h4 class="modal-title" id="myModalLabel">"""+str(len(destinationlist))+""" routes via <b>"""+key+"""</b></h4>
       </div>
       <div class="modal-body">"""
         for dest in destinationlist:

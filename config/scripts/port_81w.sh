40b280ab8c0669700aaa3294cbbf4a008eb7468b
pocki80 <pocki@pocki.at>
2018-01-29 16:52:03 +0100

    vbash

--- config/scripts/port_81w.sh
+++ config/scripts/port_81w.sh
@@ -1,13 +1,9 @@
 #!/bin/vbash
-session_env=`/bin/cli-shell-api getSessionEnv $PPID`
-session_env=$(echo $session_env | sed -e 's/active declare/active; declare/')
-eval $session_env
+eval $(echo $(/bin/cli-shell-api getSessionEnv $PPID) | sed -e 's/active declare/active; declare/')
 cli-shell-api setupSession
 
-WRAPPER=/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper
-
-$WRAPPER begin
-$WRAPPER set service gui http-port 81
-$WRAPPER commit
-$WRAPPER end
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper begin
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper set service gui http-port 81
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper commit
+/opt/vyatta/sbin/vyatta-cfg-cmd-wrapper end
 

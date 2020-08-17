fcfd9ea0562511e263c9ad19db2eb882f8a1336e
pocki80 <pocki@pocki.at>
2020-08-17 14:17:43 +0200

    fix daemon restart

--- config/letsencrypt/restartservers.sh
+++ config/letsencrypt/restartservers.sh
@@ -4,6 +4,8 @@ echo "$(date +%Y-%m-%d/%H:%M:%S.%N) Restarting daemons" >>/var/log/0xwslerestart
 sudo /sbin/start-stop-daemon --stop --pidfile /var/run/lighttpd_custom.pid >>/var/log/0xwslerestart.log 2>>/var/log/0xwslerestart.log
 sudo /sbin/start-stop-daemon --stop --pidfile /var/run/lighttpd.pid >>/var/log/0xwslerestart.log 2>>/var/log/0xwslerestart.log
 sleep 1
+running=$(ps aux | grep /etc/lighttpd/lighttpd.conf | grep www-data | awk '{print $2}')
+[ "$running" ] && sudo kill $running && sleep 1
 sudo /sbin/start-stop-daemon --start --quiet --pidfile /var/run/lighttpd.pid --exec /usr/sbin/lighttpd -- -f /etc/lighttpd/lighttpd.conf >>/var/log/0xwslerestart.log 2>>/var/log/0xwslerestart.log
 sudo /sbin/start-stop-daemon --start --quiet --pidfile /var/run/lighttpd_custom.pid --exec /usr/sbin/lighttpd -- -f /config/custom/lighttpd/lighttpd_custom.conf >>/var/log/0xwslerestart.log 2>>/var/log/0xwslerestart.log
 
